0dcc611458dac6fe076f474b7f79a576080b3697
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-07-06 08:13:34 -0600

    try to get other functions to work

--- main.js
+++ main.js
@@ -9,9 +9,36 @@ const image = document.querySelector('img')
 
 
 function imgSetter(img, url) {
-    document.getElementById(img).src = url
+    document.getEleconstmentById(img).src = url
 }
 
 imgSetter('image-1', 'https://i.ytimg.com/vi/EQAPHlEZs3E/maxresdefault.jpg')
 imgSetter('image-2', 'https: //i.ytimg.com/vi/EQAPHlEZs3E/maxresdefault.jpg')
 imgSetter('image-3', 'https://i.ytimg.com/vi/EQAPHlEZs3E/maxresdefault.jpg')
+
+
+
+const ul = document.querySelector('ul')
+
+
+funciton appendImgUL(img) {
+    ul.appendChild('img')
+}
+
+const img2 = document.createElement('img')
+
+function imgAdj(img) {
+    img.style.height = '30px';
+}
+
+function hider(ele) {
+    ele.class = 'invisible';
+}
+
+hider('numbered-item') {
+
+}
+
+function liText(writeWhat) {
+    li.innerText(writeWhat)
+}
